0b2f152cc3723285396b9f07ee47e26d814e3186
alexkarav <sashagameac@gmail.com>
2025-08-25 14:43:55 +0300

    Init

---
+++ notification_system/client/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+
+class Client(models.Model):
+    """ Пользователь системы, которому может быть отправлено уведомление """
+
+    fio = models.CharField(max_length=1024, verbose_name="ФИО Клиента")
+    telegram_id = models.IntegerField(verbose_name="ID в телеграмме", unique=True, null=True, blank=True)
+    email = models.EmailField(verbose_name="Электронная почта", null=True, blank=True)
+    phone = PhoneNumberField(blank=True, verbose_name="Номер телефона")
+
+    def __str__(self):
+        return self.fio
+    
+    class Meta:
+        verbose_name = "Клиент"
+        verbose_name_plural = "Клиенты"
+        ordering=["fio"]
